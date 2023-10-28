4b7d7ff588e1ce648e8527a5e561f9bb61b01f85
Magda <magdamoraga14@gmail.com>
2023-10-28 12:46:48 +0200

    App/AppOld?indexroutes28.10.2023

--- src/components/routes/App.tsx
+++ src/components/routes/App.tsx
@@ -1,7 +1,21 @@
 import {FC} from "react";
+import { Routes, Route } from 'react-router-dom';
 import './style/App.css';
+import { Home } from './Home';
+import { Blog } from "./Blog";
+import { Photos } from "./Photos";
+import { Profile } from "./Profile";
+
 export const App:FC = () => {
     return(
-        <>App</>
+
+        <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/blog" element={<Blog />} />
+            <Route path="/photos" element={<Photos />} />
+            <Route path="/profile" element={<Profile />} />
+        </Routes>
     );
-}
+};
+
+export default App;
